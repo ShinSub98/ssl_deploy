5ac4fc2c04612dc0fc7ac188e7bb0a3b9f8b4fb4
ShinSub98 <wjdtjq26@naver.com>
2024-01-24 13:53:07 +0900

    feat: add first page

--- ssl_project/urls.py
+++ ssl_project/urls.py
@@ -17,6 +17,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class Init(APIView):
+    def get(self, request):
+        res = {
+            "성공" :"했다"
+        }
+        return Response(res)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Init.as_view()),
 ]
