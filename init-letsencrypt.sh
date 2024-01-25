d09a480f6ad28614fd9123f792269fab41cbd1d5
Ubuntu <wjdtjq26@naver.com>
2024-01-25 05:22:24 +0000

    domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(hufsub.shop hufsub.duckdns.org)
+domains=(hufsub.duckdns.org)
 rsa_key_size=4096
 data_path="./cert/certbot"
 email="wjdtjq26@naver.com" # Adding a valid address is strongly recommended
